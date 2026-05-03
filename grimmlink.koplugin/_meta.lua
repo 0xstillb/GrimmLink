3f6296db0512b46b70cb55fa31a1d5c639e87574
0xstillb <74257775+0xstillb@users.noreply.github.com>
2026-05-03 09:42:38 +0700

    Revert "chore: bump version to 4.3.0"
    This reverts commit 004cbededff2c27e61539668f6af2bdaeebb3f66.

--- grimmlink.koplugin/_meta.lua
+++ grimmlink.koplugin/_meta.lua
@@ -1,7 +1,8 @@
 local _ = require("gettext")
+
 return {
     name = "grimmlink",
     fullname = _("GrimmLink"),
     description = _("KOReader Companion for Grimmory"),
-    version = "4.3.0",
+    version = "0.1.0-dev",
 }
