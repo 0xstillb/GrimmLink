004cbededff2c27e61539668f6af2bdaeebb3f66
0xstillb <74257775+0xstillb@users.noreply.github.com>
2026-05-03 09:35:02 +0700

    chore: bump version to 4.3.0

--- grimmlink.koplugin/_meta.lua
+++ grimmlink.koplugin/_meta.lua
@@ -1,8 +1,7 @@
 local _ = require("gettext")
-
 return {
     name = "grimmlink",
     fullname = _("GrimmLink"),
     description = _("KOReader Companion for Grimmory"),
-    version = "0.1.0-dev",
+    version = "4.3.0",
 }
--- grimmlink.koplugin/plugin_version.lua
+++ grimmlink.koplugin/plugin_version.lua
@@ -1,6 +1,6 @@
 return {
-    version = "0.1.0-dev",
-    version_type = "development",
-    git_commit = "workspace",
-    build_date = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+    version = "4.3.0",
+    version_type = "release",
+    git_commit = "727719b",
+    build_date = "2026-05-03T02:35:00Z",
 }
