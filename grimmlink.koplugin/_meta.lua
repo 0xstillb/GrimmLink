c3af8a5bafa92d6856a60ec905b61653e3eed56a
0xstillb <74257775+0xstillb@users.noreply.github.com>
2026-06-01 21:56:48 +0700

    chore(release): sync generated version files for v1.4.5

--- grimmlink.koplugin/_meta.lua
+++ grimmlink.koplugin/_meta.lua
@@ -3,5 +3,5 @@ return {
     name = "grimmlink",
     fullname = _("GrimmLink"),
     description = _("KOReader Companion for Grimmory"),
-    version = "v1.3.3",
+    version = "v1.4.5",
 }
--- grimmlink.koplugin/plugin_version.lua
+++ grimmlink.koplugin/plugin_version.lua
@@ -1,6 +1,6 @@
 return {
-    version = "v1.3.3",
+    version = "v1.4.5",
     version_type = "release",
-    git_commit = "HEAD",
-    build_date = "2026-05-19T00:00:00Z",
+    git_commit = "1daa302",
+    build_date = "2026-06-01T21:46:55+07:00",
 }
